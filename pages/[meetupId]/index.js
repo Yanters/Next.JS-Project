13ba183cf1893dd7936e583a63e5f685918ceb91
43695512 <yanter@wp.pl>
2021-09-19 23:10:19 +0200

    Detail Page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetUpDetail from "../../components/meetups/MeetUpDetail";
+
+const MeetUpDetails = () => {
+  return (
+    <MeetUpDetail
+      image="https://media.istockphoto.com/photos/woman-using-laptop-under-the-palm-tree-at-a-beach-picture-id182767952"
+      title="Beach Coding"
+      adress="Public Beach"
+      description="Working from beach"
+    />
+  );
+};
+
+export default MeetUpDetails;
